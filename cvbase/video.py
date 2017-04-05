213c54062cf71e7aead52cff20bfccd106c0bf15
zhangxc11 <xingchengzhang@sina.com>
2017-04-05 11:56:57 +0800

    fix import bug in video.py

--- cvbase/video.py
+++ cvbase/video.py
@@ -11,13 +11,13 @@ if USE_OPENCV3:
                      CAP_PROP_FRAME_COUNT, CAP_PROP_FOURCC,
                      CAP_PROP_POS_FRAMES, VideoWriter_fourcc)
 else:
-    import cv2.cv.CV_CAP_PROP_FRAME_WIDTH as CAP_PROP_FRAME_WIDTH
-    import cv2.cv.CV_CAP_PROP_FRAME_HEIGHT as CAP_PROP_FRAME_HEIGHT
-    import cv2.cv.CV_CAP_PROP_FPS as CAP_PROP_FPS
-    import cv2.cv.CV_CAP_PROP_FRAME_COUNT as CAP_PROP_FRAME_COUNT
-    import cv2.cv.CV_CAP_PROP_FOURCC as CAP_PROP_FOURCC
-    import cv2.cv.CV_CAP_PROP_POS_FRAMES as CAP_PROP_POS_FRAMES
-    import cv2.cv.CV_FOURCC as VideoWriter_fourcc
+    from cv2.cv import CV_CAP_PROP_FRAME_WIDTH as CAP_PROP_FRAME_WIDTH
+    from cv2.cv import CV_CAP_PROP_FRAME_HEIGHT as CAP_PROP_FRAME_HEIGHT
+    from cv2.cv import CV_CAP_PROP_FPS as CAP_PROP_FPS
+    from cv2.cv import CV_CAP_PROP_FRAME_COUNT as CAP_PROP_FRAME_COUNT
+    from cv2.cv import CV_CAP_PROP_FOURCC as CAP_PROP_FOURCC
+    from cv2.cv import CV_CAP_PROP_POS_FRAMES as CAP_PROP_POS_FRAMES
+    from cv2.cv import CV_FOURCC as VideoWriter_fourcc
 
 
 class Cache(object):

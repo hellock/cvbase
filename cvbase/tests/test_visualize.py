b26d217d003e5252f0b11e96b46514fea6c0525c
Kai Chen <chenkaidev@gmail.com>
2017-04-17 14:33:39 +0800

    add draw_bboxes_with_label

--- cvbase/tests/test_visualize.py
+++ cvbase/tests/test_visualize.py
@@ -1,5 +1,15 @@
-from cvbase.visualize import draw_bboxes
+from os import path
+from cvbase import draw_bboxes, read_labels
 
 
-def test_draw_bboxes():
-    assert True
+def test_read_labels():
+    label_names = ['a', 'b', 'c']
+    assert read_labels(label_names) == label_names
+    voc_labels = [
+        'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
+        'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person',
+        'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+    ]
+    assert read_labels('voc') == voc_labels
+    label_file = path.join(path.dirname(__file__), 'data/voc_labels.txt')
+    assert read_labels(label_file) == voc_labels
--- cvbase/visualize.py
+++ cvbase/visualize.py
@@ -57,3 +57,73 @@ def draw_bboxes(img, bboxes, colors=Color.green, top_k=0, thickness=1,
         cv2.waitKey(wait_time)
     if out_file is not None:
         cv2.imwrite(out_file, img)
+
+
+def voc_labels():
+    return [
+        'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
+        'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person',
+        'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+    ]
+
+
+def read_labels(file_or_labels):
+    """Read labels from file or list"""
+    if file_or_labels == 'voc':
+        return voc_labels()
+    if isinstance(file_or_labels, list):
+        label_names = file_or_labels
+    elif isinstance(file_or_labels, str):
+        with open(file_or_labels, 'r') as fin:
+            label_names = [line.rstrip('\n') for line in fin]
+    return label_names
+
+
+def draw_bboxes_with_label(img, bboxes, labels, top_k=0, bbox_color=Color.green,
+                           text_color=Color.green, thickness=1, font_scale=0.5,
+                           show=True, win_name='', wait_time=0, out_file=None):  # yapf: disable
+    """Draw bboxes with label text in image
+
+    Args:
+        img(str or ndarray): the image to be shown
+        bboxes(list or ndarray): a list of ndarray of shape (k, 4)
+        labels(str or list): label name file or list of label names
+        top_k(int): draw top_k bboxes only if positive
+        bbox_color(Color or tuple): color to draw bboxes
+        text_color(Color or tuple): color to draw label texts
+        thickness(int): thickness of bbox lines
+        font_scale(float): font scales
+        show(bool): whether to show the image
+        win_name(str): the window name
+        wait_time(int): value of waitKey param
+        out_file(str or None): the filename to write the image
+    """
+    img = read_img(img)
+    label_names = read_labels(labels)
+    if isinstance(bbox_color, Color):
+        bbox_color = bbox_color.value
+    if isinstance(text_color, Color):
+        text_color = text_color.value
+    assert len(bboxes) == len(label_names)
+    for i, _bboxes in enumerate(bboxes):
+        _bboxes = _bboxes.astype(np.int32)
+        if top_k <= 0:
+            _top_k = _bboxes.shape[0]
+        else:
+            _top_k = min(top_k, _bboxes.shape[0])
+        for j in range(_top_k):
+            left_top = (_bboxes[j, 0], _bboxes[j, 1])
+            right_bottom = (_bboxes[j, 2], _bboxes[j, 3])
+            cv2.rectangle(
+                img, left_top, right_bottom, bbox_color, thickness=thickness)
+            if _bboxes.shape[1] > 4:
+                label_text = '{}|{:.02f}'.format(label_names[i], _bboxes[j, 4])
+            else:
+                label_text = label_names[i]
+            cv2.putText(img, label_text, (_bboxes[j, 0], _bboxes[j, 1] - 2),
+                        cv2.FONT_HERSHEY_COMPLEX, font_scale, text_color)
+    if show:
+        cv2.imshow(win_name, img)
+        cv2.waitKey(wait_time)
+    if out_file is not None:
+        cv2.imwrite(out_file, img)

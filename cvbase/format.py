c7783be99049a5b423fd1162fc519fc663011541
Kai Chen <chenkaidev@gmail.com>
2017-05-28 20:15:37 +0800

    add string to bool conversion

--- cvbase/format.py
+++ cvbase/format.py
@@ -11,3 +11,12 @@ def float_list(in_list):
 def str_list(in_list):
     """Convert a list of items to a list of str"""
     return list(map(str, in_list))
+
+
+def to_bool(var):
+    if isinstance(var, str):
+        if str == '' or str.lower() == 'false':
+            return False
+        else:
+            return True
+    return bool(var)

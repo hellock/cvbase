243ec962559538043e3b2c1f32a7751315220ba6
Kai Chen <chenkaidev@gmail.com>
2017-07-07 21:10:31 +0800

    fix cmake option name

--- travis/build_opencv.sh
+++ travis/build_opencv.sh
@@ -2,7 +2,7 @@ set -x
 PYTHON_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
 
 echo "chekcing dir $OPENCV_INSTALL_PATH/lib"
-if [[ -d "$OPENCV_INSTALL_PATH/lib" ]]; then
+if [[ -f "$OPENCV_INSTALL_PATH/lib/cv2.so" ]]; then
     echo "cached opencv libs found, skip building"
     cp $OPENCV_INSTALL_PATH/lib/cv2.so $PYTHON_PACKAGES_PATH
     exit 0
@@ -16,10 +16,13 @@ mkdir build && cd build
 
 if [[ ("$OPENCV_VERSION" == 3*) && ("$TRAVIS_PYTHON_VERSION" == 2*) ]]; then
     PYV="PYTHON2"
+    NP_INC_NAME=${PYV}_NUMPY_INCLUDE_DIRS
 elif [[ ("$OPENCV_VERSION" == 3*) && ("$TRAVIS_PYTHON_VERSION" == 3*) ]]; then
     PYV="PYTHON3"
+    NP_INC_NAME=${PYV}_NUMPY_INCLUDE_DIRS
 else
     PYV="PYTHON"
+    NP_INC_NAME=${PYV}_NUMPY_INCLUDE_DIR
 fi
 
 cmake -DCMAKE_BUILD_TYPE=RELEASE \
@@ -35,7 +38,7 @@ cmake -DCMAKE_BUILD_TYPE=RELEASE \
 -D${PYV}_VERSION_STRING=$(python -c "from platform import python_version; print(python_version())") \
 -D${PYV}_INCLUDE_PATH=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
 -D${PYV}_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
--D${PYV}_NUMPY_INCLUDE_DIRS=$(python -c "import os; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
+-D${NP_INC_NAME}=$(python -c "import os; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
 -D${PYV}_NUMPY_VERSION=$(python -c "import numpy; print(numpy.version.version)") \
 ..
 

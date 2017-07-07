91597b6f6beba4ffef0857a607b6a8706868d2a8
Kai Chen <chenkaidev@gmail.com>
2017-07-07 19:43:24 +0800

    fix option name for opencv 2

--- travis/build_opencv.sh
+++ travis/build_opencv.sh
@@ -13,31 +13,34 @@ wget https://github.com/opencv/opencv/archive/$OPENCV_VERSION.tar.gz
 tar -xf $OPENCV_VERSION.tar.gz --strip-components=1
 mkdir build && cd build
 
-COMMON_OPTIONS="-DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_PATH -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_IPP=OFF -DBUILD_DOCS=OFF -DBUILD_opencv_java=OFF"
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then 
-    cmake $COMMON_OPTIONS \
-    -DBUILD_opencv_python3=OFF \
-    -DPYTHON2LIBS_FOUND=ON \
-    -DPYTHON2_EXECUTABLE=$(python -c "import sys; print(sys.executable)") \
-    -DPYTHON2_VERSION_STRING=$(python -c "from platform import python_version; print(python_version())") \
-    -DPYTHON2_INCLUDE_PATH=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
-    -DPYTHON2_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
-    -DPYTHON2_NUMPY_INCLUDE_DIRS=$(python -c "import os; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
-    -DPYTHON2_NUMPY_VERSION=$(python -c "import numpy; print(numpy.version.version)") \
-    ..
-else 
-    cmake $COMMON_OPTIONS \
-    -DBUILD_opencv_python2=OFF \
-    -DPYTHON3LIBS_FOUND=ON \
-    -DPYTHON3_EXECUTABLE=$(python -c "import sys; print(sys.executable)") \
-    -DPYTHON3_VERSION_STRING=$(python -c "from platform import python_version; print(python_version())") \
-    -DPYTHON3_INCLUDE_PATH=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
-    -DPYTHON3_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
-    -DPYTHON3_NUMPY_INCLUDE_DIRS=$(python -c "import os; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
-    -DPYTHON3_NUMPY_VERSION=$(python -c "import numpy; print(numpy.version.version)") \
-    ..
+if [[ ("$OPENCV_VERSION" == 3* ) && ("$TRAVIS_PYTHON_VERSION" == 2*) ]]; then
+    PYV="PYTHON2"
+elif [[ ("$OPENCV_VERSION" == 3* ) && ("$TRAVIS_PYTHON_VERSION" == 3*) ]]; then
+    PYV="PYTHON3"
+else
+    PYV="PYTHON"
 fi
-make -j
+
+cmake $COMMON_OPTIONS \
+-DCMAKE_BUILD_TYPE=RELEASE \
+-DCMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_PATH \
+-DBUILD_SHARED_LIBS=OFF \
+-DBUILD_TESTS=OFF \
+-DBUILD_PERF_TESTS=OFF \
+-DWITH_IPP=OFF \
+-DBUILD_DOCS=OFF \
+-DBUILD_opencv_java=OFF
+-DBUILD_opencv_python3=OFF \
+-D${PYV}LIBS_FOUND=ON \
+-D${PYV}_EXECUTABLE=$(python -c "import sys; print(sys.executable)") \
+-D${PYV}_VERSION_STRING=$(python -c "from platform import python_version; print(python_version())") \
+-D${PYV}_INCLUDE_PATH=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+-D${PYV}_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+-D${PYV}_NUMPY_INCLUDE_DIRS=$(python -c "import os; import numpy.distutils; print(os.pathsep.join(numpy.distutils.misc_util.get_numpy_include_dirs()))") \
+-D${PYV}_NUMPY_VERSION=$(python -c "import numpy; print(numpy.version.version)") \
+..
+
+make -j2
 make install
 cp $PYTHON_PACKAGES_PATH/cv2*.so $OPENCV_INSTALL_PATH/lib/cv2.so
 cd $TRAVIS_BUILD_DIR
